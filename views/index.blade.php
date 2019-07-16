f8fbb752dd683a2d0cb148dfe9d3a5c742992391
Duncan McClean <duncan@mcclean.co.uk>
2019-07-16 18:12:03 +0100

    🐤 Initial commit

---
+++ views/index.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Systatic</title>
+		<meta charset="UTF-8">
+	  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<style>
+			body {
+			  margin: 0px;
+			  padding: 0px;
+			  background-color: #ffb305;
+			  font-weight: 10;
+			  color: #ffffff;
+				font-family: 'system-ui', 'BlinkMacSystemFont', '-apple-system', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'sans-serif';
+			}
+
+			div {
+			  display: flex;
+			  flex-direction: column;
+			  align-items: center;
+			  justify-content: center;
+			  height: 100vh;
+			}
+
+			h1 {
+			  font-size: 80px;
+			  line-height: 1;
+			}
+
+			nav {
+			  display: flex;
+			  flex-direction: row;
+			  align-items: center;
+			}
+
+			a {
+			  margin-left: 10px;
+			  margin-right: 10px;
+			  color: #ffffff;
+			  text-decoration: none;
+			}
+		</style>
+	</head>
+	<body>
+	  <div>
+	    <h1>Systatic</h1>
+	    <nav>
+	      <a href="https://systatic.co/docs">Documentation</a>
+	      <a href="https://github.com/damcclean/systatic">Github</a>
+	      <a href="https://discord.gg/zSg3MHv">Discord</a>
+	    </nav>
+	  </div>
+	</body>
+</html>
